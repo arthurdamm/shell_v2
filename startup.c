627919bf29ff6b1eb4c82172abc47cfb93781d74
Arthur Damm <indigoarthur@gmail.com>
2020-02-25 22:28:48 +0000

    Add comment for Betty

--- startup.c
+++ startup.c
@@ -4,6 +4,7 @@
  * open_file - opens a file descriptor, exits on failure
  * @info: the parameter struct
  * @name: name of file to open
+ * @silent: if true don't print errors
  * Return: fd of file
  */
 int open_file(info_t *info, char *name, int silent)
